d18c42439e121b18a44b75d8829400a1219693e9
satrioiyooo <satriowibowo004@gmail.com>
2024-01-08 14:30:46 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("polymer")))
+	http.HandleFunc("/api/mahasiswa", user)
+	fmt.Println("Listening on Port :9090")
+	http.ListenAndServe(":9090", nil)
+}
+
+type lepkom struct {
+	Nama   string `json:"nama_mahasiswa"`
+	Kursus string `json:"kursus_mahasiswa"`
+	Foto   string `json:"foto_mahasiswa"`
+}
+
+var data_mahasiswa = []lepkom{
+	{
+		Nama:   "Budi",
+		Kursus: "Funda Web",
+		Foto:   "img/gambar1.jpg",
+	},
+	{
+		Nama:   "Ilham",
+		Kursus: "Golang Beginner",
+		Foto:   "img/gambar1.jpg",
+	},
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type","application/json")
+	if r.Method == http.MethodGet {
+
+		result, err := json.Marshal(data_mahasiswa)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(result)
+		return
+	}
+}
